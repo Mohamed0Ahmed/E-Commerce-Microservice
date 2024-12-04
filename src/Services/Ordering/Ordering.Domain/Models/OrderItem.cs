69896506542044fbda730c526e48d50826d15858
Mohamed Ahmed <mahmmaad96@gmail.com>
2024-12-04 15:03:32 +0200

    Develop Ordering Domain Layer

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,45 @@
+﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.Enums;
+using Ordering.Domain.Events;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Domain.Models
+{
+    public class OrderItem : Entity<OrderItemId>
+    {
+        internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+        {
+            Id = OrderItemId.Of(Guid.NewGuid());
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public OrderId OrderId { get; private set; } = default!;
+        public ProductId ProductId { get; private set; } = default!;
+        public int Quantity { get; private set; } = default!;
+        public decimal Price { get; private set; } = default!;
+    }
+
+      public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+        {
+            var order = new Order
+            {
+                Id = id,
+                CustomerId = customerId,
+                OrderName = orderName,
+                ShippingAddress = shippingAddress,
+                BillingAddress = billingAddress,
+                Payment = payment,
+                Status = OrderStatus.Pending
+            };
+
+            order.AddDomainEvent(new OrderCreatedEvent(order));
+
+            return order;
+        }
+
+
+    }
+}
