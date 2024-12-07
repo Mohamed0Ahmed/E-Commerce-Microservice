35704b3b96f76ce6ebddecfd68709bda33488110
Mohamed Ahmed <mahmmaad96@gmail.com>
2024-12-07 15:29:44 +0200

    API Layer Exposing Minimal APi Endpoints With Carter

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,17 +1,31 @@
-﻿namespace Ordering.API
+﻿using BuildingBlocks.Exceptions.Handler;
+using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Ordering.API
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
-           
+            services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddCarter();
+            services.AddHealthChecks().AddSqlServer(configuration.GetConnectionString("Database")!);
 
             return services;
         }
 
         public static WebApplication UseApiServices(this WebApplication app)
         {
-         
+            app.MapCarter();
+            app.UseExceptionHandler(options => { });
+
+            app.UseHealthChecks("/health",
+          new HealthCheckOptions
+          {
+              ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+          });
 
             return app;
         }
